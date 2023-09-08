d34872fb5d0dd33698ade886fa4253b325a47812
Anmol Gaud <uranmol03@gmail.com>
2023-09-09 00:16:04 +0530

    tailwind configured

--- devjobs-web-main/tailwind.config.js
+++ devjobs-web-main/tailwind.config.js
@@ -5,6 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily : {
+      sans: ['Open Sans', sans-serif],
+      poppins : ['Poppins', sans-serif],
+    },
+    colors : {
+      'white' : '#ffffff',
+      'offWhite' : '#eeeeee',
+      'darkOffWhite' : '#d4d4d4',
+      'lightGray' : '#37414f',
+      'darkGray' : '#222831',
+      'blue' : '#0092ca',
+    },
     extend: {},
   },
   plugins: [],
